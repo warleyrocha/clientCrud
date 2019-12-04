adc51d49d6739ac78449874157a6ac3028721b71
Warley Valney Silva Rocha <wsrochab@gmail.com>
2019-12-04 06:03:07 -0300

    Spring Initializr

---
+++ src/test/java/br/com/brasilprev/clientCrud/ClientCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brasilprev.clientCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
